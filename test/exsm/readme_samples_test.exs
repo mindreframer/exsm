193e654743cce6480da7f427592f9503ef06a879
Roman Heinrich <roman.heinrich@gmail.com>
2022-03-09 21:11:00 +0100

    Add tests for readme example

---
+++ test/exsm/readme_samples_test.exs
@@ -0,0 +1,31 @@
+defmodule Exsm.ReadmeSamplesTest do
+  use ExUnit.Case
+
+  defmodule YourProject.UserStateMachine do
+    use Exsm,
+      field: :state,
+      states: ["created", "partial", "completed", "canceled", "paused", "restarted"],
+      transitions: %{
+        "created" => ["partial", "completed"],
+        "partial" => "completed",
+        "completed" => "restarted",
+        "*" => "canceled"
+      }
+  end
+
+  alias YourProject.UserStateMachine
+
+  describe "hello" do
+    test "works" do
+      user = %{state: "created"}
+      assert Exsm.transition_to(user, UserStateMachine, "partial") == {:ok, %{state: "partial"}}
+    end
+
+    test "fails on undeclared transitions" do
+      user = %{state: "created"}
+
+      assert Exsm.transition_to(user, UserStateMachine, "restarted") ==
+               {:error, "Transition from: created, to: restarted isn't declared."}
+    end
+  end
+end
